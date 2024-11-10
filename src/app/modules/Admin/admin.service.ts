9b45bc7f21a4317042a7fd9322c415658861c879
unknown <shefata69@gmail.com>
2024-11-10 20:32:28 +0600

    pagination and sorting

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,31 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { adminSeacrhAbleFields } from "./admin.constant";
 
 const prisma = new PrismaClient()
+
+const calculatePagination = (options: {
+    page?: number,
+    limit?: number,
+    sortBy?: string,
+    sortOrder?: string
+}) => {
+    const page: number = Number(options.page) || 1;
+    const limit: number = Number(options.limit) || 10;
+    const skip: number = (Number(page) - 1) * limit;
+
+    const sortBy = options.sortBy || "createdAt"
+    const sortOrder = options.sortOrder || "desc"
+
+    return {
+        page,
+        limit,
+        skip,
+        sortBy,
+        sortOrder
+    }
+}
+
 const getAllFromDB = async (params: any, options: any) => {
-    const { limit, page } = options;
+    const { page, limit, skip } = calculatePagination(options);
     const { searchTerm, ...fillterData } = params
     const andConditions: Prisma.AdminWhereInput[] = []
     if (params.searchTerm) {
@@ -28,8 +51,13 @@ const getAllFromDB = async (params: any, options: any) => {
     const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
     const result = await prisma.admin.findMany({
         where: whereConditions,
-        skip: (Number(page) - 1) * limit,
-        take: Number(limit)
+        skip,
+        take: limit,
+        orderBy: options.sortBy && options.sortOrder ? {
+            [options.sortBy]: options.sortOrder
+        } : {
+            createdAt: "desc"
+        }
     });
     return result
 }
