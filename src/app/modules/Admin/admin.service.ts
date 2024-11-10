8bef51a90d43466d1e119b07b9f12e63efaa22cd
unknown <shefata69@gmail.com>
2024-11-10 17:51:30 +0600

    add filtering on admin

---
+++ src/app/modules/Admin/admin.service.ts
@@ -0,0 +1,36 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+const getAllFromDB = async (params: any) => {
+    const {searchTerm, ...fillterData} = params
+    const andConditions: Prisma.AdminWhereInput[] = []
+    const adminSeacrhAbleFields = ['name', 'email']
+    if (params.searchTerm) {
+        andConditions.push({
+            OR: adminSeacrhAbleFields.map(field => ({
+                [field]: {
+                    contains: params.searchTerm,
+                    mode: "insensitive"
+                }
+            }))
+        })
+    }
+    if(Object.keys(fillterData).length > 0){
+        andConditions.push({
+            AND: Object.keys(fillterData).map(key=> ({
+                [key] : {
+                    equals: fillterData[key]
+                }
+            }))
+        })
+    }
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
+    const result = await prisma.admin.findMany({
+        where: whereConditions
+    });
+    return result
+}
+
+export const AdminService = {
+    getAllFromDB
+}
