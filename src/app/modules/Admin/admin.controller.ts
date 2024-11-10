6d4ed2dddc8f077e4cacee72c6df1f5e62708600
unknown <shefata69@gmail.com>
2024-11-10 20:08:53 +0600

    pagination

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -6,7 +6,8 @@ import { adminFilterableFields } from "./admin.constant"
 const getAllFromDB = async (req: Request, res: Response) => {
     try {
         const filters = pick(req.query, adminFilterableFields)
-        const result = await AdminService.getAllFromDB(filters)
+        const options = pick(req.query, ['limit', 'page'])
+        const result = await AdminService.getAllFromDB(filters, options)
         res.status(200).json({
             success: true,
             message: "Admin data fetched!",
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,9 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { adminSeacrhAbleFields } from "./admin.constant";
 
 const prisma = new PrismaClient()
-const getAllFromDB = async (params: any) => {
-    const {searchTerm, ...fillterData} = params
+const getAllFromDB = async (params: any, options: any) => {
+    const { limit, page } = options;
+    const { searchTerm, ...fillterData } = params
     const andConditions: Prisma.AdminWhereInput[] = []
     if (params.searchTerm) {
         andConditions.push({
@@ -15,10 +16,10 @@ const getAllFromDB = async (params: any) => {
             }))
         })
     }
-    if(Object.keys(fillterData).length > 0){
+    if (Object.keys(fillterData).length > 0) {
         andConditions.push({
-            AND: Object.keys(fillterData).map(key=> ({
-                [key] : {
+            AND: Object.keys(fillterData).map(key => ({
+                [key]: {
                     equals: fillterData[key]
                 }
             }))
@@ -26,7 +27,9 @@ const getAllFromDB = async (params: any) => {
     }
     const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
     const result = await prisma.admin.findMany({
-        where: whereConditions
+        where: whereConditions,
+        skip: (Number(page) - 1) * limit,
+        take: Number(limit)
     });
     return result
 }
