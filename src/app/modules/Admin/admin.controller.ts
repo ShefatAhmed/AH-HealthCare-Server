09eac39a59426efe8eb64f8a3232c5df00234f94
unknown <shefata69@gmail.com>
2024-11-11 13:00:29 +0600

    admin crud operations

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -23,6 +23,82 @@ const getAllFromDB = async (req: Request, res: Response) => {
     }
 }
 
+const getByIdFromDB = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const result = await AdminService.getByIdFromDB(id);
+        res.status(200).json({
+            success: true,
+            message: "Admin data fetched by id!",
+            data: result
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err?.name || "Something went wrong",
+            error: err
+        })
+    }
+}
+
+const updateIntoDB = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const result = await AdminService.updateIntoDB(id, req.body);
+        res.status(200).json({
+            success: true,
+            message: "Admin data updated!",
+            data: result
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err?.name || "Something went wrong",
+            error: err
+        })
+    }
+}
+
+const deleteFromDB = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const result = await AdminService.deleteFromDB(id);
+        res.status(200).json({
+            success: true,
+            message: "Admin data deleted!",
+            data: result
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err?.name || "Something went wrong",
+            error: err
+        })
+    }
+}
+
+const softDeleteFromDB = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const result = await AdminService.softDeleteFromDB(id);
+        res.status(200).json({
+            success: true,
+            message: "Admin data deleted!",
+            data: result
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err?.name || "Something went wrong",
+            error: err
+        })
+    }
+}
+
 export const AdminController = {
-    getAllFromDB
+    getAllFromDB,
+    getByIdFromDB,
+    updateIntoDB,
+    deleteFromDB,
+    softDeleteFromDB
 }
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,9 +1,7 @@
-import { Prisma } from "@prisma/client";
+import { Admin, Prisma, UserStatus } from "@prisma/client";
 import { adminSeacrhAbleFields } from "./admin.constant";
 import { paginationHelper } from "../../../helpers/paginationHelper";
 import prisma from "../../../shared/prisma";
-
-
 
 const getAllFromDB = async (params: any, options: any) => {
     const { page, limit, skip } = paginationHelper.calculatePagination(options);
@@ -28,6 +26,9 @@ const getAllFromDB = async (params: any, options: any) => {
             }))
         })
     }
+    andConditions.push({
+        isDeleted: false
+    })
     const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
     const result = await prisma.admin.findMany({
         where: whereConditions,
@@ -52,6 +53,87 @@ const getAllFromDB = async (params: any, options: any) => {
     }
 }
 
+const getByIdFromDB = async (id: string): Promise<Admin | null> => {
+    const result = await prisma.admin.findUnique({
+        where: {
+            id,
+            isDeleted: false
+        }
+    })
+    return result
+}
+
+const updateIntoDB = async (id: string, data: Partial<Admin>): Promise<Admin> => {
+    await prisma.admin.findUniqueOrThrow({
+        where: {
+            id,
+            isDeleted: false
+        }
+    })
+    const result = await prisma.admin.update({
+        where: {
+            id
+        },
+        data
+    })
+    return result
+}
+
+const deleteFromDB = async (id: string): Promise<Admin | null> => {
+    await prisma.admin.findUniqueOrThrow({
+        where: {
+            id
+        }
+    })
+    const result = await prisma.$transaction(async (transactionClient) => {
+        const adminDeletedData = await transactionClient.admin.delete({
+            where: {
+                id: id
+            }
+        });
+        await transactionClient.user.delete({
+            where: {
+                email: adminDeletedData.email
+            }
+        })
+        return adminDeletedData;
+    });
+    return result
+}
+
+const softDeleteFromDB = async (id: string): Promise<Admin | null> => {
+    await prisma.admin.findUniqueOrThrow({
+        where: {
+            id,
+            isDeleted: false
+        }
+    })
+    const result = await prisma.$transaction(async (transactionClient) => {
+        const adminDeletedData = await transactionClient.admin.update({
+            where: {
+                id: id
+            },
+            data: {
+                isDeleted: true
+            }
+        });
+        await transactionClient.user.update({
+            where: {
+                email: adminDeletedData.email
+            },
+            data: {
+                status: UserStatus.DELETED
+            }
+        })
+        return adminDeletedData;
+    });
+    return result
+}
+
 export const AdminService = {
-    getAllFromDB
+    getAllFromDB,
+    getByIdFromDB,
+    updateIntoDB,
+    deleteFromDB,
+    softDeleteFromDB
 }
