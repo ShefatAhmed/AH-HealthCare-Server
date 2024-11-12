616a81c2c370d00575cf63bf65fa14b5eaee4679
unknown <shefata69@gmail.com>
2024-11-12 11:52:20 +0600

    complete async

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -4,16 +4,7 @@ import pick from "../../../shared/pick"
 import { adminFilterableFields } from "./admin.constant"
 import sendResponse from "../../../shared/sendResponse"
 import httpStatus from "http-status"
-
-const catchAsync = (fn: RequestHandler) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            await fn(req, res, next)
-        } catch (err) {
-            next(err)
-        }
-    }
-}
+import catchAsync from "../../../shared/catchAsync"
 
 const getAllFromDB: RequestHandler = catchAsync(async (req, res) => {
     const filters = pick(req.query, adminFilterableFields)
