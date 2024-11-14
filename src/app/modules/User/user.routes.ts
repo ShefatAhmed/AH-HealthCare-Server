c4765ada8add559ef98160fd4f71d5e39640f57b
unknown <shefata69@gmail.com>
2024-11-14 11:25:50 +0600

    uploading image

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,8 +2,9 @@ import express from "express"
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../../helpers/fileUploader";
 const router = express.Router();
 
-router.post('/', auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), userController.createAdmin)
+router.post('/', auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), fileUploader.upload.single('file'), userController.createAdmin)
 
 export const UserRoutes = router;
