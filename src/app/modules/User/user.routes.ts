7f9a8fdca56ca3d957136b9c2a7fd71616e71e0b
unknown <shefata69@gmail.com>
2024-11-14 15:20:04 +0600

    start working doctor feature

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -6,7 +6,11 @@ import { fileUploader } from "../../../helpers/fileUploader";
 import { userValidation } from "./user.validation";
 const router = express.Router();
 
-router.post('/', auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), fileUploader.upload.single('file'), (req: Request, res: Response, next: NextFunction) => {
+router.post('/create-admin', auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), fileUploader.upload.single('file'), (req: Request, res: Response, next: NextFunction) => {
+    req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data))
+    return userController.createAdmin(req, res, next)
+})
+router.post('/create-doctor', auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), fileUploader.upload.single('file'), (req: Request, res: Response, next: NextFunction) => {
     req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data))
     return userController.createAdmin(req, res, next)
 })
