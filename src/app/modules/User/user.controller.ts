2b490de84eaa75d5e839234b95a25752ba6a5c8b
unknown <shefata69@gmail.com>
2024-11-14 13:19:18 +0600

    profile image uploads

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,21 +1,16 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
 
-const createAdmin = async (req: Request, res: Response) => {
-    try {
-        const result = await userService.createAdmin(req.body);
-        res.status(200).json({
-            success: true,
-            message: "Admin created succesfully!",
-            data: result
-        })
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong",
-            error: err
-        })
-    }
+const createAdmin = async (req: Request, res: Response, next?: unknown) => {
+    const result = await userService.createAdmin(req);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin creted successfully!",
+        data: result
+    })
 }
 
 export const userController = {
--- src/helpers/fileUploader.ts
+++ src/helpers/fileUploader.ts
@@ -1,5 +1,7 @@
 import multer from "multer"
-import path from "path"
+import path, { resolve } from "path"
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -12,6 +14,31 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
+cloudinary.config({
+    cloud_name: 'dsolupxw1',
+    api_key: '845715583541723',
+    api_secret: 'LfMvgtCedRAc_DPxpCZq1Kb4ON4'
+});
+
+
+const uploadToCloudinary = async (file: any) => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader
+            .upload(
+                file.path, { public_id: file.originalname },
+                (error, result) => {
+                    fs.unlinkSync(file.path)
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve(result)
+                    }
+                }
+            )
+    })
+}
+
 export const fileUploader = {
-    upload
+    upload,
+    uploadToCloudinary
 }
