df1ad645c52f521ff628fe996fc9418f49bf7e40
unknown <shefata69@gmail.com>
2024-11-17 12:17:13 +0600

    start creation of schedule

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,5 +1,23 @@
-const insertIntoDB = async (payload : any) => {
-    console.log(payload);
+import { addHours, format } from "date-fns"
+const insertIntoDB = async (payload: any) => {
+    const { statDate, endDate, statTime, endTime } = payload
+    const currentDate = new Date(statDate)
+    const lastDate = new Date(endDate)
+    while (currentDate <= lastDate) {
+        const statDateTime = new Date(
+            addHours(
+                `${format(currentDate, 'yyyy-MM-dd')}`,
+                Number(statTime.split(':')[0])
+            )
+        )
+        const endDateTime = new Date(
+            addHours(
+                `${format(lastDate, 'yyyy-MM-dd')}`,
+                Number(endTime.split(':')[0])
+            )
+        )
+        console.log(endDateTime);
+    }
 }
 
 
