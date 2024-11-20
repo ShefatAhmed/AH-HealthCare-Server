fbddf962d53b7af9dac7e1bb13e60ec63560c9e7
unknown <shefata69@gmail.com>
2024-11-20 13:08:14 +0600

    get all review

--- src/app/modules/Review/review.controller.ts
+++ src/app/modules/Review/review.controller.ts
@@ -4,6 +4,8 @@ import sendResponse from "../../../shared/sendResponse";
 import { Request, Response } from "express";
 import { ReviewServices } from "./review.service";
 import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
+import { reviewFilterableFields } from "./review.contant";
 
 const insertIntoDB = catchAsync(async (req: Request & {user?: IAuthUser}, res: Response) => {
     const user = req.user
@@ -16,6 +18,20 @@ const insertIntoDB = catchAsync(async (req: Request & {user?: IAuthUser}, res: R
     });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, reviewFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await ReviewServices.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Reviews retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
 export const ReviewController = {
-    insertIntoDB
+    insertIntoDB,
+    getAllFromDB
 };
